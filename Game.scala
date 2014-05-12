6405cd22681b5821179d622fd22700c2d5fd167f
Tim Wilkens <timwilkensf1@gmail.com>
2014-05-11 21:45:12 -0700

    Abstract. Abstract.

--- Game.scala
+++ Game.scala
@@ -14,11 +14,16 @@ object Play extends SimpleSwingApplication {
   val darkerBlue    = new AWTColor (0, 96, 128)
   val darkestBlue   = new AWTColor (0, 85, 102)
 
+  val colors = Array(lightestBlue, lightBlue, blue, darkBlue, darkerBlue, darkestBlue)
+  var colorIndex = 1
+
   val scoreLabel = new ScoreLabel
   scoreLabel.text = "Score: "
 
   val score = new ScoreLabel
   score.text = "0"
+
+  val randomIterator = scala.util.Random
 
   val header = new BoxPanel(Orientation.Horizontal) {
     contents += scoreLabel
@@ -27,40 +32,37 @@ object Play extends SimpleSwingApplication {
 
   val labels = 1 to 16 map { i => new GameLabel }
 
-  val row1 = new BoxPanel(Orientation.Horizontal) {
-    contents += labels(0)
-    contents += labels(1)
-    contents += labels(2)
-    contents += labels(3)
+  def generateRow(i: Int): BoxPanel = {
+    new BoxPanel(Orientation.Horizontal) {
+      contents += labels(i)
+      contents += labels(i + 1)
+      contents += labels(i + 2)
+      contents += labels(i + 3)
+    }
   }
 
-  val row2 = new BoxPanel(Orientation.Horizontal) {
-    contents += labels(4)
-    contents += labels(5)
-    contents += labels(6)
-    contents += labels(7)
-  }
-
-  val row3 = new BoxPanel(Orientation.Horizontal) {
-    contents += labels(8)
-    contents += labels(9)
-    contents += labels(10)
-    contents += labels(11)
-  }
-
-  val row4 = new BoxPanel(Orientation.Horizontal) {
-    contents += labels(12)
-    contents += labels(13)
-    contents += labels(14)
-    contents += labels(15)
-  }
+  val row1 = generateRow(0)
+  val row2 = generateRow(4)
+  val row3 = generateRow(8)
+  val row4 = generateRow(12)
 
   val icon = "o"
-  var position = 0
+  var position = randomIterator.nextInt(16)
   labels(position).text = icon
+  labels(position).background = lightestBlue 
+
+  def handleMove(i: Int, f: (Int, Int) => Int): Unit  = {
+    labels(position).text = null
+     position = f(position, i)
+     labels(position).text = icon
+     colorIndex = (colorIndex + 1) % 5
+     labels(position).background = colors(colorIndex)
+     score.text = ((score.text.toInt) + 1).toString
+  }
 
   def top = new MainFrame {
     title = "2048"
+    background = cream
     centerOnScreen()
     preferredSize = new Dimension(400, 450)
     maximumSize = new Dimension(400, 450)
@@ -73,8 +75,6 @@ object Play extends SimpleSwingApplication {
       contents += row3
       contents += row4
 
-      background = cream
-
       focusable = true
       requestFocus
 
@@ -82,32 +82,19 @@ object Play extends SimpleSwingApplication {
       reactions += {
         case KeyReleased(_, Key.Left, _, _) =>
           if ((position != 0) && (position != 4) && (position != 8) && (position != 12)) {
-            labels(position).text = null
-            position = position - 1
-            labels(position).text = icon
-            labels(position).background = lightBlue
-            score.text = ((score.text.toInt) + 1).toString
+            handleMove(1, (x, y) => x - y)
           }
         case KeyReleased(_, Key.Right, _, _) =>
           if ((position != 3) && (position != 7) && (position != 11) && (position != 15)) {
-            labels(position).text = null
-            position = position + 1
-            labels(position).text = icon
-            labels(position).background = blue
+            handleMove(1, (x, y) => x + y)
           }
         case KeyReleased(_, Key.Up, _, _) =>
           if ((position != 0) && (position != 1) && (position != 2) && (position != 3)) {
-            labels(position).text = null
-            position = position - 4
-            labels(position).text = icon
-            labels(position).background = darkBlue
+            handleMove(4, (x, y) => x - y)
           }
         case KeyReleased(_, Key.Down, _, _) =>
           if ((position != 12) && (position != 13) && (position != 14) && (position != 15)) {
-            labels(position).text = null
-            position = position + 4
-            labels(position).text = icon
-            labels(position).background = darkestBlue
+            handleMove(4, (x, y) => x + y)
           }
       }
     }
@@ -124,7 +111,7 @@ class GeneralLabel extends Label {
 class ScoreLabel extends GeneralLabel {
   minimumSize = new Dimension(200,50)
   maximumSize = minimumSize
-  font = new Font("Arial", 0, 24)
+  font = new Font("Scala", 0, 24)
 }
 
 class GameLabel extends GeneralLabel {
