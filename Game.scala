ec715b4ccb303a88536a746aec1f339c57a74071
Tim Wilkens <timwilkensf1@gmail.com>
2014-05-11 23:27:04 -0700

    Add computer play.

--- Game.scala
+++ Game.scala
@@ -46,44 +46,49 @@ object Play extends SimpleSwingApplication {
   val row3 = generateRow(8)
   val row4 = generateRow(12)
 
-  var position = randomIterator.nextInt(16)
-  labels(position).text = "2"
-  labels(position).background = lightestBlue 
+  def computerPlay(): Unit = {
+    val available = (0 to 15).filter(x => labels(x).background == cream)
+    if (available.size == 0) return
+    var position = 0
 
-  position = randomIterator.nextInt(16)
-  labels(position).text = "2"
-  labels(position).background = lightBlue 
+    if (available.size == 1) {
+      position = available(0)
+    } else {
+      position = randomIterator.nextInt(available.size - 1)
+    }
 
-  position = randomIterator.nextInt(16)
-  labels(position).text = "2"
-  labels(position).background = blue 
+    labels(position).text = "2"
+    labels(position).background = lightestBlue
+  }
 
-  position = randomIterator.nextInt(16)
-  labels(position).text = "2"
-  labels(position).background = darkBlue 
+  // Side Effects!
+  computerPlay()
 
-  def handleMove(position: Int, i: Int, f: (Int, Int) => Int, max: Int): Unit  = {
-     if (labels(position).background == cream) return
-     val prevText = labels(position).text
-     val prevColor = labels(position).background
-     var newPosition = f(position, i)
-     if (labels(newPosition).background != cream) return
+
+  def handleMove(position: Int, i: Int, f: (Int, Int) => Int, max: Int): Int  = {
+    if (labels(position).background == cream) return 0
+    val prevText = labels(position).text
+    val prevColor = labels(position).background
+    var newPosition = f(position, i)
+    if (labels(newPosition).background != cream) return 0
         
-     if (max > 1) {
+    if (max > 1) {
        // Probably a much nicer recursive solution.
-       for (x <- 0 to max - 2) {
-         val temp = f(newPosition, i)
-         if ((temp <= 15) && (temp >= 0) && (labels(temp).background == cream)) {
-           newPosition = temp
-         }
-       }
-     }
+      for (x <- 0 to max - 2) {
+        val temp = f(newPosition, i)
+        if ((temp <= 15) && (temp >= 0) && (labels(temp).background == cream)) {
+          newPosition = temp
+        }
+      }
+    }
 
-     labels(position).text = null
-     labels(position).background = cream
-     labels(newPosition).text = prevText
-     labels(newPosition).background = prevColor
-     score.text = ((score.text.toInt) + 1).toString
+    labels(position).text = null
+    labels(position).background = cream
+    labels(newPosition).text = prevText
+    labels(newPosition).background = prevColor
+    score.text = ((score.text.toInt) + 1).toString
+
+    return 1 
   }
 
   def top = new MainFrame {
@@ -105,8 +110,10 @@ object Play extends SimpleSwingApplication {
       requestFocus
 
       listenTo(keys)
+      // Seriously violating DRY...
       reactions += {
         case KeyReleased(_, Key.Left, _, _) =>
+          var counter = 0
           for (s <- 0 to 15) {
             if ((s != 0) && (s != 4) && (s != 8) && (s != 12)) {
               var maxMoves = 0;
@@ -117,27 +124,58 @@ object Play extends SimpleSwingApplication {
               } else {
                 maxMoves = 3
               }
-              handleMove(s, 1, (x, y) => x - y, maxMoves)
+              counter += handleMove(s, 1, (x, y) => x - y, maxMoves)
             }
           }
+          if (counter > 0) computerPlay()
         case KeyReleased(_, Key.Right, _, _) =>
+          var counter = 0
           for (s <- (0 to 15).reverse) {
             if ((s != 3) && (s != 7) && (s != 11) && (s != 15)) {
-                handleMove(s, 1, (x, y) => x + y, 0)
+              var maxMoves = 0;
+              if ((s == 2) || (s == 6) || (s == 10) || (s == 14)) {
+                maxMoves = 1
+              } else if ((s == 1) || (s == 5) || (s == 9) || (s == 13)) {
+                maxMoves = 2
+              } else {
+                maxMoves = 3
+              }
+              counter += handleMove(s, 1, (x, y) => x + y, maxMoves)
             }
           }
+          if (counter > 0) computerPlay()
         case KeyReleased(_, Key.Up, _, _) =>
+          var counter = 0
           for (s <- (0 to 12 by +4) ++ (1 to 13 by +4) ++ (2 to 14 by +4) ++ (3 to 15 by +4)) {
             if ((s != 0) && (s != 1) && (s != 2) && (s != 3)) {
-              handleMove(s, 4, (x, y) => x - y, 0)
+              var maxMoves = 0;
+              if (s < 8) {
+                maxMoves = 1
+              } else if (s < 12) {
+                maxMoves = 2
+              } else {
+                maxMoves = 3
+              }
+              counter += handleMove(s, 4, (x, y) => x - y, maxMoves)
             }
           }
+          if (counter > 0) computerPlay()
         case KeyReleased(_, Key.Down, _, _) =>
+          var counter = 0
           for (s <- (12 to 0 by -4) ++ (13 to 1 by -4) ++ (14 to 2 by -4) ++ (15 to 3 by -4)) {
             if ((s != 12) && (s != 13) && (s != 14) && (s != 15)) {
-              handleMove(s, 4, (x, y) => x + y, 0)
+              var maxMoves = 0;
+              if (s > 7) {
+                maxMoves = 1
+              } else if (s > 4) {
+                maxMoves = 2
+              } else {
+                maxMoves = 3
+              }
+              counter += handleMove(s, 4, (x, y) => x + y, maxMoves)
             }
           }
+          if (counter > 0) computerPlay()
       }
     }
   }
